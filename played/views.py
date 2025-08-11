02b86bdd64b4bafb7abfc580690e9d9110f662e3
AbraWalker <abraxaswalker@hotmail.com>
2025-08-11 20:37:17 +0100

    add authentication and aesthetic changes

--- played/views.py
+++ played/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 # Create your views here.
 class ReviewList(generic.ListView):
     queryset = Post.objects.all()
     template_name = "index.html"
+
+def review_details(request, slug):
+    queryset = Post.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, "played/review_list.html", {"review": post},)
